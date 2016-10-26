0cd7426cd8aace8d7d4063a84a2ca0382abb1262
PiotrKulpa <p.kulpa@onet.eu>
2016-10-26 23:40:40 +0200

    setInterval added

--- js/main.js
+++ js/main.js
@@ -6,12 +6,17 @@ var intervalMachine = {
     intensiveTime: function () {return document.getElementById('intensive-time').value; },
     restTime: function () {return document.getElementById('rest-time').value; },
     cooldownTime: function () {return document.getElementById('cooldown-time').value; },
+    repeatNumber: function () {return document.getElementById('repeat-number').value; },
+    clockUi: function () {return document.getElementById('clock-ui').innerHTML; },
     //ustawia interwały i zwraca pozostały czas do końca
     startMachine: function () {
-        var i = 1;
-        setInterval(function () { alert(i++); }, 1000);
-        var test = this.prepareTime();
-        console.log(test);
+        var elapsedTime = parseInt(this.prepareTime(), 10) + parseInt(this.intensiveTime(), 10) * parseInt(this.repeatNumber(), 10) + parseInt(this.restTime(), 10) * parseInt(this.repeatNumber(), 10) + parseInt(this.cooldownTime(), 10);
+        setInterval(function () { 
+            elapsedTime--;
+            this.clockUi = elapsedTime;//niedziala
+        }, 1000);
+        
+        console.log(elapsedTime);
     }
 	
 };
