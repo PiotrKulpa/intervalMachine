dd48c58ad33cf98e07597e45a66c0a549106c175
Piotr Kulpa <p.kulpa@onet.eu>
2016-10-26 21:54:05 +0200

    adding button & inputs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-
+'use strict';
 
 var intervalMachine = {
 	name: 'Interval Machine',
-	prepareTime: function () {return document.getElementById('prepare-time').innerHTML; },
-    intensiveTime: function () {return document.getElementById('intensive-time').innerHTML; },
-    restTime: function () {return document.getElementById('rest-time').innerHTML; },
-    cooldownTime: function () {return document.getElementById('cooldown-time').innerHTML; },
+	prepareTime: function () {return document.getElementById('prepare-time').value; },
+    intensiveTime: function () {return document.getElementById('intensive-time').value; },
+    restTime: function () {return document.getElementById('rest-time').value; },
+    cooldownTime: function () {return document.getElementById('cooldown-time').value; },
     //ustawia interwały i zwraca pozostały czas do końca
-    startMachine: function () {}
+    startMachine: function () {
+        var i = 1;
+        setInterval(function () { alert(i++); }, 1000);
+        var test = this.prepareTime();
+        console.log(test);
+    }
 	
 };
-console.log(intervalMachine.intensiveTime());
+
+var startButton = document.getElementById('start-intervals');
+startButton.onclick = function () {intervalMachine.startMachine(); };
