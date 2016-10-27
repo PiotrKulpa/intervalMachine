4000089c604ac56426777490976c4d59e017186b
Piotr Kulpa <p.kulpa@onet.eu>
2016-10-27 12:46:16 +0200

    counters added

--- js/main.js
+++ js/main.js
@@ -7,19 +7,33 @@ var intervalMachine = {
     restTime: function () {return document.getElementById('rest-time').value; },
     cooldownTime: function () {return document.getElementById('cooldown-time').value; },
     repeatNumber: function () {return document.getElementById('repeat-number').value; },
-    clockUi: function () {return document.getElementById('clock-ui').innerHTML; },
-    //ustawia interwały i zwraca pozostały czas do końca
+    elapsedTime: function () {return parseInt(this.prepareTime(), 10) + parseInt(this.intensiveTime(), 10) * parseInt(this.repeatNumber(), 10) + parseInt(this.restTime(), 10) * parseInt(this.repeatNumber(), 10) + parseInt(this.cooldownTime(), 10); },
+    shiftTime: function () {return this.elapsedTime() - parseInt(this.prepareTime(), 10); },
     startMachine: function () {
-        var elapsedTime = parseInt(this.prepareTime(), 10) + parseInt(this.intensiveTime(), 10) * parseInt(this.repeatNumber(), 10) + parseInt(this.restTime(), 10) * parseInt(this.repeatNumber(), 10) + parseInt(this.cooldownTime(), 10);
-        setInterval(function () { 
-            elapsedTime--;
-            this.clockUi = elapsedTime;//niedziala
+        var elapsedTimeShow = this.elapsedTime();
+        var prepareTimeShow = this.prepareTime();
+        var i = 0;
+        var endSound = document.getElementById('end-sound');
+        var runCountdown = setInterval(function () { 
+            i++;
+            if (i === prepareTimeShow) {
+                endSound.play();
+            }
+            //console.log(this.prepareTime() );
+            elapsedTimeShow--;
+            document.getElementById('clock-ui').innerHTML = elapsedTimeShow;
+            if (elapsedTimeShow === 0) {
+                clearInterval(runCountdown);
+                
+                endSound.play();
+            }
         }, 1000);
         
-        console.log(elapsedTime);
+        
     }
 	
 };
 
 var startButton = document.getElementById('start-intervals');
 startButton.onclick = function () {intervalMachine.startMachine(); };
+
